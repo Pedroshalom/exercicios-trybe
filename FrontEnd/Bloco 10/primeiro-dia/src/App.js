06ec8d324fac0633de4d27029242dd3791539bc6
Pedro <pedroshalommrn@hotmail.com>
2022-05-17 16:09:22 -0300

    projeto pessoal

---
+++ FrontEnd/Bloco 10/primeiro-dia/src/App.js
@@ -0,0 +1,49 @@
+// import logo from './assets/logo.png';
+import React from 'react';
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+    <h2> <strong>𝑨𝒏𝒅𝒓𝒆𝒛𝒂 𝑺𝒂𝒏𝒕𝒐𝒔 𝑨𝒕𝒆𝒍𝒊𝒆</strong></h2>
+    <hr/>
+    <p>Empreendedor(a)</p>
+    <span>❣Ⓒⓡⓞⓒⓗⓔ Ⓜⓞⓓⓔⓡⓝⓞ❣</span>
+    <p><strong>𝑯𝒂𝒏𝒅𝒎𝒂𝒅𝒆 𝒑𝒐𝒓: 𝑨𝒏𝒅𝒓𝒆𝒛𝒂 𝑺𝒂𝒏𝒕𝒐𝒔</strong></p>
+    <p>📦𝑽𝒆𝒏𝒅𝒂𝒔 𝒐𝒏𝒍𝒊𝒏𝒆;</p>
+    <p>📲 𝑷𝒂𝒓𝒂 𝒎𝒂𝒊𝒔 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒄𝒐𝒆𝒔 𝒂𝒄𝒆𝒔𝒔𝒆 𝒐 𝒍𝒊𝒏𝒌 𝒅𝒐 𝒘𝒂𝒕𝒕𝒔 👇</p>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    
+    <section>
+      <footer>
+      <hr/>
+        Todos os direitos resrvados a {''}{''}  𝑨𝒏𝒅𝒓𝒆𝒛𝒂 𝑺𝒂𝒏𝒕𝒐𝒔 𝑨𝒕𝒆𝒍𝒊𝒆 🧶
+        </footer>
+        </section>
+    </div>
+    )
+  }
+
+}
+
+export default App;
